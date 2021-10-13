dd69bce1e3641e2650482da5e2e258aa9c68b2e1
Dayeane <dayeane85@gmail.com>
2021-10-12 21:37:40 -0700

    functions:continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(text='roller-skate') {
+    return `This Saturday, I want to ${text}!`;
+}
+
+function mondayWork(text="go to the office") {
+    return `This Monday, I will ${text}.`
+}
+
+function wrapAdjective (adjective='*') {
+    return function (text="special") {
+        return `You are ${adjective}${text}${adjective}!`
+    }
+}
